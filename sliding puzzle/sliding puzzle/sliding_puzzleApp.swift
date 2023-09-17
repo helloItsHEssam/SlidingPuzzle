009beba4a90a0a37f4bd7e2a91878c79834ed8f0
Hessam Mahdiabadi <67460597+iamHEssam@users.noreply.github.com>
2023-09-17 12:29:31 +0330

    add xocde project

---
+++ sliding puzzle/sliding puzzle/sliding_puzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sliding_puzzleApp.swift
+//  sliding puzzle
+//
+//  Created by Hessam Mahdiabadi on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct sliding_puzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
